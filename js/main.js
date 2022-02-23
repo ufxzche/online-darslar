a07f9bfc298af1a39afcc814c1e49402fcb7e7e4
Masanov <masanov3167@gmail.com>
2022-02-23 15:39:47 +0500

    udygsbhs

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('.toggle').addEventListener('click', function(){
+    document.querySelector('.toggle').classList.toggle('active');
+    document.querySelector('.header-nav').classList.toggle('animate');
+    
+})
